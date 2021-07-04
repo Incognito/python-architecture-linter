2df88d05ededba3a8dd1cae490cbe10f95c3ff26
Brian Graham <Brian@statagroup.com>
2021-07-04 16:08:04 +0200

    Those are empty files

--- python_architecture_linter/tests/test_linter.py
+++
@@ -1,4 +0,0 @@
-from python_architecture_linter.linter import Linter
-
-def test_definition():
-    assert True == True
--- python_architecture_linter/tests/test_node_navigators.py
+++
@@ -1,4 +0,0 @@
-from python_architecture_linter.node_navigators import project_to_files, project_to_file, project_to_file_filtered, file_to_ast, ast_node_to_specific_children
-
-def test_definition():
-    assert True == True
