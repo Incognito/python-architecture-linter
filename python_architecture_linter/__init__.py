325f8be25862c9dc4a98d6f89f16650826263a3c
Brian Graham <Brian@statagroup.com>
2021-07-10 17:25:42 +0200

    ValidationResult should be public API

--- python_architecture_linter/__init__.py
+++ python_architecture_linter/__init__.py
@@ -1,4 +1,5 @@
+from python_architecture_linter.domain_objects.validation_result import ValidationResult
 from python_architecture_linter.linter import Linter
 from python_architecture_linter.tree_structure import Structure
 
-__all__ = ["Structure", "Linter"]
+__all__ = ["Structure", "Linter", "ValidationResult"]
